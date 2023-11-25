f9ecf355122c577906f41ff659baf409adcf37a7
Adriano Miranda <adrjmiranda@gmail.com>
2023-11-25 17:54:50 -0300

    chore: added course messages.

--- app/classes/CourseMessage.php
+++ app/classes/CourseMessage.php
@@ -4,4 +4,25 @@ namespace app\classes;
 
 class CourseMessage
 {
+  const ERR_FAIL_CREATE = '';
+  const ERR_FAIL_UPDATE = '';
+  const ERR_FAIL_DELETE = '';
+
+  const ERR_INVALID_TITLE = 'Título inválido.';
+  const ERR_INVALID_DESCRIPTION = 'Descrição inválido.';
+  const ERR_INVALID_IMAGE_TYPE = 'Tipo de imagem inválido.';
+  const ERR_INVALID_IMAGE_LENGTH = 'Comprimento de imagem inválido.';
+
+  const ERR_INVALID_TOPIC_TITLE = '';
+  const ERR_INVALID_TOPIC_CONTENT = '';
+
+  const ERR_INVALID_QUIZZ_TITLE = '';
+
+  const ERR_INVALID_QUESTION_TEXT = '';
+  const ERR_INVALID_RIGHT_ANSWER = '';
+
+  const ERR_INVALID_LETTER = '';
+  const ERR_INVALID_ALTERNATIVE = '';
+
+  const ERR_INVALID_SCORE = '';
 }
