1e7b009ed1dad7315772d8948f3c0a72bef8d047
Adriano Miranda <adrjmiranda@gmail.com>
2023-11-26 19:11:25 -0300

    chore: added create form to course.

--- app/Controllers/CourseController.php
+++ app/Controllers/CourseController.php
@@ -3,20 +3,50 @@
 namespace app\Controllers;
 
 use app\Models\CourseModel;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
 
 class CourseController extends Controller
 {
-  public function index(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+  protected ResponseFactoryInterface $responseFactory;
+  protected Twig $twig;
+  private array $data;
+  private string $path;
+
+  public function __construct(ResponseFactoryInterface $responseFactory, Twig $twig, string $baseURL, string $csrfToken)
   {
-    $course = new CourseModel();
-    $courses = $course->all();
+    $this->responseFactory = $responseFactory;
+    $this->twig = $twig;
 
-    $this->view('/pages/courses/home.html.twig', [
-      'courses' => $courses
-    ]);
+    $this->model = new CourseModel();
 
-    return $response;
+    $this->path = '/pages/courses/';
+
+    $this->data = [];
+    $this->data['base_url'] = $baseURL;
+    $this->data['csrf_token'] = $csrfToken;
+  }
+
+  public function create(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+  {
+    $this->path .= 'create_course.html.twig';
+    $this->data['page_title'] = 'NetLearnHub | Aprenda de graça TI';
+    $this->data['title'] = '';
+    $this->data['description'] = '';
+    $this->data['err_image'] = false;
+    $this->data['err_title'] = false;
+    $this->data['err_description'] = false;
+    $this->data['session_message'] = '';
+
+    return $this->twig->render($response, $this->path, $this->data);
+  }
+
+  public function store(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+  {
+    $params = $request->getParsedBody();
+
+    return $this->twig->render($response, $this->path, $this->data);
   }
 }
--- app/routes/administratorRoutes.php
+++ app/routes/administratorRoutes.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../functions/authentication.php';
 
+use app\Controllers\CourseController;
 use Slim\Routing\RouteCollectorProxy;
 use app\Controllers\AdministratorController;
 
@@ -10,28 +11,38 @@ $twig = $dependencies['twig'];
 $baseURL = $dependencies['base_url'];
 $csrfToken = $dependencies['csrf_token'];
 
-$controller = new AdministratorController($responseFactory, $twig, $baseURL, $csrfToken);
-$table = $controller->getTable();
+$administratorController = new AdministratorController($responseFactory, $twig, $baseURL, $csrfToken);
+$courseController = new CourseController($responseFactory, $twig, $baseURL, $csrfToken);
 
-$app->group('/admin', function (RouteCollectorProxy $group) use ($controller) {
-  $group->get('/login', function ($request, $response, $args) use ($controller) {
-    return $controller->index($request, $response, $args);
+$table = $administratorController->getTable();
+
+$app->group('/admin', function (RouteCollectorProxy $group) use ($administratorController, $courseController) {
+  $group->get('/login', function ($request, $response, $args) use ($administratorController) {
+    return $administratorController->index($request, $response, $args);
   });
 
-  $group->post('/login', function ($request, $response, $args) use ($controller) {
-    return $controller->login($request, $response, $args);
+  $group->post('/login', function ($request, $response, $args) use ($administratorController) {
+    return $administratorController->login($request, $response, $args);
   })->add(function ($request, $handler) {
     return verifyCSRFToken($request, $handler);
   });
 
-  $group->get('/dashboard', function ($request, $response, $args) use ($controller) {
-    return $controller->dashboard($request, $response, $args);
+  $group->get('/dashboard', function ($request, $response, $args) use ($administratorController) {
+    return $administratorController->dashboard($request, $response, $args);
   });
 
-  $group->get('/logout', function ($request, $response, $args) use ($controller) {
-    return $controller->logout($request, $response, $args);
+  $group->get('/logout', function ($request, $response, $args) use ($administratorController) {
+    return $administratorController->logout($request, $response, $args);
   });
 
+  // course
+  $group->get('/course/create', function ($request, $response, $args) use ($courseController) {
+    return $courseController->create($request, $response, $args);
+  });
+
+  $group->post('/course/create', function ($request, $response, $args) use ($courseController) {
+    return $courseController->store($request, $response, $args);
+  });
 })->add(function ($request, $handler) use ($table) {
   return verifyTokenMiddleware($request, $handler, $table);
 });
