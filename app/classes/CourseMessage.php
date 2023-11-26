f8281dbe3cf392ba906ced6aed91b5a0826cbcb2
Adriano Miranda <adrjmiranda@gmail.com>
2023-11-26 00:30:47 -0300

    chore: added info message to courses and users.

--- app/classes/CourseMessage.php
+++ app/classes/CourseMessage.php
@@ -2,27 +2,36 @@
 
 namespace app\classes;
 
+/**
+ * Class CourseMessage
+ *
+ * This class stores course-related message constants to maintain consistent messages.
+ */
 class CourseMessage
 {
-  const ERR_FAIL_CREATE = '';
-  const ERR_FAIL_UPDATE = '';
-  const ERR_FAIL_DELETE = '';
+  // Error messages related to course operations
+  const ERR_FAIL_CREATE = 'Falha ao tentar criar curso.';
+  const ERR_FAIL_UPDATE = 'Falha ao tentar atualizar curso.';
+  const ERR_FAIL_DELETE = 'Falha ao tentar deletar curso.';
 
+  // Error messages related to course details validation
   const ERR_INVALID_TITLE = 'Título inválido.';
   const ERR_INVALID_DESCRIPTION = 'Descrição inválido.';
-  const ERR_INVALID_IMAGE_TYPE = 'Tipo de imagem inválido.';
-  const ERR_INVALID_IMAGE_LENGTH = 'Comprimento de imagem inválido.';
+  const ERR_INVALID_IMAGE_TYPE = 'Tipo de imagem inválido. Somente jpg, jpeg ou png.';
+  const ERR_INVALID_IMAGE_LENGTH = 'Tamanho da imagem inválido. Deve ter no máximo 2 MB.';
 
-  const ERR_INVALID_TOPIC_TITLE = '';
-  const ERR_INVALID_TOPIC_CONTENT = '';
+  // Error messages related to course topics
+  const ERR_INVALID_TOPIC_TITLE = 'Título do tópico inválido.';
+  const ERR_INVALID_TOPIC_CONTENT = 'Conteúdo do tópico inválido.';
 
-  const ERR_INVALID_QUIZZ_TITLE = '';
+  // Error messages related to quizzes
+  const ERR_INVALID_QUIZZ_TITLE = 'Título do quiz inválido.';
 
-  const ERR_INVALID_QUESTION_TEXT = '';
-  const ERR_INVALID_RIGHT_ANSWER = '';
+  // Error messages related to questions in quizzes
+  const ERR_INVALID_QUESTION_TEXT = 'Texto da questão inválido.';
+  const ERR_INVALID_RIGHT_ANSWER = 'A resposta selecionada como correta é inválida.';
 
-  const ERR_INVALID_LETTER = '';
-  const ERR_INVALID_ALTERNATIVE = '';
-
-  const ERR_INVALID_SCORE = '';
+  // Error messages related to alternatives in quizzes
+  const ERR_INVALID_LETTER = 'Alternativa inválida. Deve ser uma letra do alfabeto.';
+  const ERR_INVALID_ALTERNATIVE = 'Texto para alternativa inválida.';
 }
--- app/classes/UserMessage.php
+++ app/classes/UserMessage.php
@@ -5,27 +5,29 @@ namespace app\classes;
 /**
  * Class UserMessage
  *
- * This class stores user messages constants to maintain consistent messages.
+ * This class stores user-related message constants to maintain consistent messages.
  */
 class UserMessage
 {
-  // Error messages related to authentication
-  const ERR_EMAIL_NOT_FOUND = 'Email não encontrado.';
-  const ERR_INCORRECT_PASS = 'Senha incorreta.';
-  const ERR_LOGIN = 'Houve um problema e não foi possivel fazer o login agora.';
+  // Error messages related to email lookup
+  const ERR_EMAIL_NOT_FOUND = 'Email não encontrado. Não existe um usuário com esse email.';
+  const ERR_INCORRECT_PASS = 'A seenha está incorreta.';
+  const ERR_LOGIN = 'Houve um problema e não foi possível fazer o login agora.';
 
+  // Error messages related to email registration
+  const ERR_EMAIL_ALREADY_REGISTERED = 'E-mail already registered.';
   const ERR_REGISTER = 'Houve um erro e não foi possível fazer o registro agora.';
 
-  // Error messages related to user input validation
-  const ERR_INVALID_FIRST_NAME = 'Nome inválido.';
-  const ERR_INVALID_LAST_NAME = 'Sobrenome inválido.';
+  // Error messages related to user details validation
+  const ERR_INVALID_FIRST_NAME = 'Nome inválido. Somente letras e espaços em branco.';
+  const ERR_INVALID_LAST_NAME = 'Sobrenome inválido. Somente letras e espaços em branco.';
   const ERR_INVALID_EMAIL = 'E-mail inválido.';
-  const ERR_INVALID_PASS = 'Senha inválida.';
+  const ERR_INVALID_PASS = 'Senha inválida. Deve ter de 8 a 20 caracteres (com letras e números).';
   const ERR_INVALID_PASS_CONFIRMATION = 'A confirmação da senha não corresponde.';
-  const ERR_INVALID_IMAGE_TYPE = 'Tipo de imagem inválido.';
-  const ERR_INVALID_IMAGE_LENGTH = 'Comprimento de imagem inválido.';
+  const ERR_INVALID_IMAGE_TYPE = 'Tipo de imagem inválido. Somente jpg, jpeg ou png.';
+  const ERR_INVALID_IMAGE_LENGTH = 'Tamanho da imagem inválido. Deve ter no máximo 2 MB.';
   const ERR_INVALID_DESCRIPTION = 'Descrição inválida.';
 
-  // Error messages related to comments
+  // Error message related to comment
   const ERR_INVALID_COMMENT = 'Comentário inválido.';
 }
