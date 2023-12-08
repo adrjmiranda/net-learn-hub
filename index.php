047c29c0cdfe89e6056f893fa97a0287d051ee59
Adriano Miranda <adrjmiranda@gmail.com>
2023-12-08 11:13:54 -0300

    refactor: modified login page to user.

--- index.php
+++ index.php
@@ -5,9 +5,6 @@ $dependencies = require_once __DIR__ . '/bootstrap.php';
 
 $app = $dependencies['slim_app'];
 
-$gg = $dependencies[GlobalValues::G_CSRF_TOKEN];
-var_dump($gg);
-
 require_once __DIR__ . '/app/routes/staticFilesRoutes.php';
 require_once __DIR__ . '/app/routes/administratorRoutes.php';
 require_once __DIR__ . '/app/routes/courseRoutes.php';
